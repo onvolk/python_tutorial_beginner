90ae30215eb8aebe09392a6cf3811b80f4bea88a
Olha Volk <on.volk@gmail.com>
2019-04-22 22:15:54 +0200

    Sets new

--- src/lesson02/test_sets.py
+++ src/lesson02/test_sets.py
@@ -5,10 +5,17 @@ import unittest
 
 
 class TestSets(unittest.TestCase):
-    def test_set(self):
+    def test_set_creation(self):
+        set_a = {1, 2, 3, 4, 5, 6, 7}
+        set_b = set([1, 2, 3, 4])
+        difference = set_a.difference(set_b)
+        self.assertSetEqual(difference, {5, 6, 7})
+
+    def test_remove_duplicats(self):
         nums = [1, 2, 1, 1, 1, 3, 4]
         self.assertListEqual(list(set(nums)), [1, 2, 3, 4])
 
 
 if __name__ == '__main__':
     unittest.main()
+
