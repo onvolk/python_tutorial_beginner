f0b64a2a60f1b3f3d433a63c0a822abdf8abac0a
Olha Volk <on.volk@gmail.com>
2019-05-07 13:31:46 +0200

    Column rename, re-arrange

--- src/lesson05/test_select_by_column.py
+++ src/lesson05/test_select_by_column.py
@@ -11,17 +11,17 @@ class TestPandasSelectByColumn(unittest.TestCase):
 
     CURRENT_FOLDER = os.path.dirname(os.path.abspath(__file__))
 
-    def test_select_by_name(self):
+    def test_select_column_by_name(self):
         data = pd.read_csv('{}\\fixtures\customers.csv'.format(self.CURRENT_FOLDER))
         result = data[['City', 'CustomerName']]
         result.to_csv('{}\\out\select_by_name.csv'.format(self.CURRENT_FOLDER))
 
-    def test_select_by_number(self):
+    def test_select_column_by_number(self):
         data = pd.read_csv('{}\\fixtures\customers.csv'.format(self.CURRENT_FOLDER))
         result = data[[1, 2]]
         result.to_csv('{}\\out\select_by_number.csv'.format(self.CURRENT_FOLDER))
 
-    def test_select_by_attribute(self):
+    def test_select_column_by_attribute(self):
         data = pd.read_excel('{}\\fixtures\customers.xlsx'.format(self.CURRENT_FOLDER), sheet_name='Sheet1')
         result = data.CustomerName
         result.to_excel('{}\\out\select_by_attribute.xlsx'.format(self.CURRENT_FOLDER))
